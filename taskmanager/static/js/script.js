00cec283d2100f5178a97d9582115e49eb54b761
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-04-10 11:47:17 +0000

    Updates

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // side nav
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
+
+    // date picker
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+    // category select
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
+    // collapsibles
+    let collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
   });
